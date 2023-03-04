a239e28fd3e8c0ee2396bca7b3dc7dac375b626a
Mehedi Hasan <mhjibon.info@gmail.com>
2023-03-04 22:29:33 +0600

    done upto project

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// DoNot show api key on your js file 
+
+const API_KEY = `692319813044bc1f839d91c6eb673e46`;
+
+const loadTemperature = city => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data));
+};
+
+const  displayTemperature = data => {
+    // const temperature = document.getElementById("temperature");
+    // temperature.innerText = data.main.temp;
+    setInnerTextById("temperature" , data.main.temp);
+    setInnerTextById("condition", data.weather[0].main);
+    // console.log(data.weather[0].main);
+};
+
+const setInnerTextById = (id, text) =>{
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
+};
+
+document.getElementById("btn-search").addEventListener("click", function(){
+    const searchField = document.getElementById("search-field");
+    const city = searchField.value;
+    // set city 
+    document.getElementById("city").innerText = city;
+
+    loadTemperature(city);
+});
+
+loadTemperature("dhaka");
